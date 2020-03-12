f646b8f704de69c6b08ce6f8f38ae50aae6491ba
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2020-03-12 09:02:44 -0400

    Adding local storage in apiWikipedia file

--- src/pages/apiWikipedia/index.js
+++ src/pages/apiWikipedia/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './style.css';
 
@@ -9,33 +10,49 @@ class WikiSearch extends Component {
           content: [],
           language: 'en'
      }
+     componentDidMount() {
+          this.setState({
+               content: this.getDataLocalStorage()[0],
+               query: this.getDataLocalStorage()[1]
+          })
+     }
 
-     handleSearch = (event) => {
+     handleSearch = (event) => (
           this.setState({
                query: event.target.value
           })
-     }
+     );
 
-     handleSelecte = (event) => {
+     handleSelecte = (event) => (
           this.setState({
                language: event.target.value
           })
+     );
+
+     addDataLocalStorage = (data, query) => {
+          let d = [];
+          d.push(data);
+          d.push(query);
+          localStorage.setItem('Data', JSON.stringify(d));
      }
+
+     getDataLocalStorage = () => (
+          JSON.parse(localStorage.getItem('Data')) || []
+     );
 
      handleSubmitSearch = () => {
           const { query, language } = this.state;
-
           const url = `https://${language}.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${query}`;
           fetch(url)
                .then(res => res.json())
                .then(data => data.query.search.length === 0 ?
-                    alert('this content does not exist') :
-                    this.setState({
-                         content: data.query.search
-                    })
+                    alert('this content does not exist') : (
+                         this.setState({
+                              content: data.query.search
+                         }), this.addDataLocalStorage(data.query.search, query))
                ).catch(() => console.log('An error occurred'));
-
      }
+
      render() {
           return (
                <div className='login-box'>
@@ -45,15 +62,18 @@ class WikiSearch extends Component {
                               <option value='en'>English</option>
                               <option value='es'>Spanish</option>
                          </select>
-                         <input type='text' onChange={event => this.handleSearch(event)} />
+                         <input type='text' value={this.state.query} onChange={event => this.handleSearch(event)} />
                          <input type='submit' value='Search' onClick={() => this.handleSubmitSearch()} />
                          <p>{this.state.content.length || ''}</p>
                     </div>
                     <hr />
                     <div className='login-box2'>{this.state.content.map((search, index) => <div key={`article-${index}`}>
-                         <a href={encodeURI(`https://en.wikipedia.org/wiki/${search.title}`)}>
-                              <h3>{search.title}</h3>
-                         </a>
+                         <Link to={{
+                              pathname: '/WikiContent',
+                              state: {
+                                   url: encodeURI(`https://en.wikipedia.org/wiki/${search.title}`)
+                              }
+                         }}><h3>{search.title}</h3></Link>
                          <p> {search.snippet.replace(/<[^>]*>?/g, '')} </p>
                          <hr />
                     </div>)}
@@ -64,4 +84,3 @@ class WikiSearch extends Component {
 }
 
 export default WikiSearch;
-
